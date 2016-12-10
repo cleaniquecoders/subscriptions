536d39ce0e3a40e02b158f00adc1b97c092924dc
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-12-10 16:00:47 +0800

    fixed route & controller

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::get('subscribe/{id}', 'SubscriptionsController@subscribe')->name('subscriptions.subscribe');
-    Route::get('unsubscribed', 'SubscriptionsController@unsubscribed')->name('subscriptions.unsubscribed');
-    Route::get('expired', 'SubscriptionsController@expired')->name('subscriptions.expired');
+    Route::get('subscribe/{id}', 'CleaniqueCoders\Subscriptions\Http\Controllers\SubscriptionController@subscribe')->name('subscriptions.subscribe');
+    Route::get('unsubscribed', 'CleaniqueCoders\Subscriptions\Http\Controllers\SubscriptionController@unsubscribed')->name('subscriptions.unsubscribed');
+    Route::get('expired', 'CleaniqueCoders\Subscriptions\Http\Controllers\SubscriptionController@expired')->name('subscriptions.expired');
 
     Route::group(['middleware' => ['subscription']], function () {
-        Route::resource('packages', 'SubscriptionsController');
+        Route::resource('subscriptions', 'CleaniqueCoders\Subscriptions\Http\Controllers\SubscriptionController');
     });
 });
