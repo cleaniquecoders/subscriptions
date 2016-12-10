1d04640a523c9d2c7c38b28ad9b718787e7b2b8c
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-12-10 14:07:26 +0800

    initial commit

---
+++ src/SubscriptionsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CleaniqueCoders\Subscriptions;
+
+use Illuminate\Support\ServiceProvider;
+
+class SubscriptionsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
